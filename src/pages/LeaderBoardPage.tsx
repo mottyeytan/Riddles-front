19efab765ad8da28baa10e84db19b3756aa6ee4e
שם המשתמש שלך <your-email@example.com>
2025-09-02 22:08:10 +0300

    add game funcionlty

--- src/pages/LeaderBoardPage.tsx
+++ src/pages/LeaderBoardPage.tsx
@@ -1,10 +1,5 @@
 import PlayerCard from "../componets/LeaderBoardPage/PlayerCard.tsx";
 
-const getLeaderboard = async () => {
-    const response = await fetch('http://localhost:3000/leaderboard');
-    const data = await response.json();
-    return data;
-}
 
 export default function LeaderBoardPage(){
     return (
