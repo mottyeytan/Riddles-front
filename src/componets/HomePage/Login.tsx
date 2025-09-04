923acefcd4e974cede68f0779b561b43ad627d91
שם המשתמש שלך <your-email@example.com>
2025-09-04 12:56:43 +0300

    Fix login and add netlify config

--- src/componets/HomePage/Login.tsx
+++ src/componets/HomePage/Login.tsx
@@ -13,9 +13,8 @@ export default function Login(){
     const { login } = useAuth();
 
     async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
-        console.log('Login function called!'); // DEBUG
         event.preventDefault();
-        console.log('Form submitted with:', { username, password }); // DEBUG
+        
 
         try{
             const response = await fetch('https://riddle-server.onrender.com/auth/login', {
@@ -41,7 +40,6 @@ export default function Login(){
                 setError(data.message || 'Invalid username or password');
             }
         } catch (error) {
-            console.log('Login error:', error); // DEBUG
             setError(error instanceof Error ? error.message : 'Invalid username or password');
             console.log(error);
         }
